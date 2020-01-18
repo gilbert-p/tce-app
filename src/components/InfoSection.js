824bbb1996249757d71cf48026b2ead88f32a67d
epoxi117 <gplacid2@calstatela.edu>
2020-01-18 10:41:22 -0800

    Added info card view with grid

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -3,16 +3,18 @@ import "../styles/info_section.scss";
 
 const InfoSection = () => {
   return (
-    <div className="p-2 info-section">
-      <div className="d-flex flex-column">
-        <div id="info-title" className="p-2">
-          <h2>Amazon Delivery Service Partner</h2>
-        </div>
-        <div id="info-body" className="p-2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
-          perferendis inventore, assumenda ea blanditiis placeat sequi eos
-          corporis velit minima, aspernatur aliquid sunt dicta natus ipsum eius
-          atque quas nihil!
+    <div className="info-section">
+      <div className="container">
+        <div className="row">
+          <div id="info-title" className="col-lg-12 col-md-auto col-sm">
+            <h2>Amazon Delivery Service Partner</h2>
+          </div>
+          <div id="info-body" className="col-lg-12 col-md-auto col-sm">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex
+            perferendis inventore, assumenda ea blanditiis placeat sequi eos
+            corporis velit minima, aspernatur aliquid sunt dicta natus ipsum
+            eius atque quas nihil!
+          </div>
         </div>
       </div>
     </div>
