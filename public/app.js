d56f130a7af0f4e5cac9a41c669967f6da7bd4d7
epoxi117 <gplacid2@calstatela.edu>
2020-02-03 08:50:21 -0800

    working on enabling authenticated users to read/write data

---
+++ public/app.js
@@ -0,0 +1,28 @@
+//make sure to initialize firebase instance when document is loaded
+document.addEventListener("DOMContentLoaded", event => {
+  const app = firebase.app();
+});
+
+const googleLogin = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  firebase
+    .auth()
+    .signInWithPopup(provider)
+    .then(result => {
+      const user = result.user;
+      document.write(`Welcome ${user.displayName}`);
+      console.log(user);
+    })
+    .catch(console.log);
+
+  const db = firebase.firestore();
+
+  const contactPosts = db.collection("contact-messages").doc("test-contact");
+
+  contactPosts.get().then(doc => {
+    const data = doc.data();
+    document.write(data.title + `<br>`);
+    document.write(data.createdAt);
+  });
+};
